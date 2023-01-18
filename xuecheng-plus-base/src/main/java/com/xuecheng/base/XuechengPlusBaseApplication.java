e1917ad8218f6e5430b809044d7901d2929ebea7
Wuxinyue <2467637323@qq.com>
2023-01-18 15:45:16 +0800

    提交父工程、基础工程的基础代码（pom.xml）

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/XuechengPlusBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XuechengPlusBaseApplication.class, args);
+	}
+
+}
